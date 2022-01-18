d7bec53a6c899e3bda4c89fbda2b1caa195c17d4
ChaitanyaWarad <94101135+ChaitanyaWarad@users.noreply.github.com>
2022-01-18 16:51:55 +0530

    Add files via upload

---
+++ SpringBootHibernateRestMethodsMysql/src/main/java/com/javatpoint/SpringBootAppStart.java
@@ -0,0 +1,12 @@
+package com.javatpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppStart {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppStart.class, args);
+	}
+}
